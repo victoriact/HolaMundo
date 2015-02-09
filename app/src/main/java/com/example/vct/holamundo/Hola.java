8344f2bfd27e3b94dcbd7ac89d287f14de362b2b
victoriact <victoria.cont@gmial.com>
2015-02-08 19:22:45 -0600

    test of textview

--- app/src/main/java/com/example/vct/holamundo/Hola.java
+++ app/src/main/java/com/example/vct/holamundo/Hola.java
@@ -4,5 +4,6 @@ package com.example.vct.holamundo;
  * Created by Victoria on 08/02/2015.
  */
 public class Hola {
-    int a = 0;
+    private int a = 0;
+
 }
--- app/src/main/java/com/example/vct/holamundo/MainActivity.java
+++ app/src/main/java/com/example/vct/holamundo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvwHolaMundo =  (TextView) this.findViewById(R.id.tvwHolaMundo);
+        tvwHolaMundo.setText("Hola Simon :) ");
+
         int a = 0;
     }
 
